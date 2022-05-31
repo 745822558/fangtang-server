c5f239a68f35973e2977759f0a60ebdbad3b1478
liAnXin <745822558@qq.com>
2022-05-31 14:53:11 +0800

    更新名称

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:8-jdk-slim
 MAINTAINER yuepong.com
 ARG JAR_FILE
-WORKDIR /opt/myBot
+WORKDIR /opt
 COPY ${JAR_FILE} app.jar
 ENV JAVA_OPTS=""
 ENTRYPOINT ["bash", "-c", "java $JAVA_OPTS -jar app.jar"]
--- src/main/java/com/lax/ftbot/FangTangApplication.java
+++ src/main/java/com/lax/ftbot/FangTangApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
-public class SpringMiraiServerApplication {
+public class FangTangApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMiraiServerApplication.class, args);
+        SpringApplication.run(FangTangApplication.class, args);
     }
 
 }
