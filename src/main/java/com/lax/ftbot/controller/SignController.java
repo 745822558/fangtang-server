1de5bace94e1dadaf27c91fd53ed750a33e5137b
liAnXin <745822558@qq.com>
2022-06-01 18:01:13 +0800

    修改参数

--- src/main/java/com/lax/ftbot/controller/SignController.java
+++ src/main/java/com/lax/ftbot/controller/SignController.java
@@ -5,22 +5,22 @@ import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.lax.ftbot.service.SignService;
 import com.lax.ftbot.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sign")
+@Slf4j
 public class SignController {
 
     @Autowired
     SignService signService;
 
 
-    @RequestMapping
-    public R getSign(String functionId,@RequestBody String body) {
+    @GetMapping
+    public R getSign(@RequestParam String functionId, @RequestBody String body) {
+        log.info("functionId:{},body:{}", functionId, body);
         if (StrUtil.isEmpty(functionId) || StrUtil.isEmpty(body)) {
             return R.fail(-1, "参数异常！");
         }
